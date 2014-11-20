93b95447bffc397a67c342995de1ffa8f3c172b4
Suthon <suthon@gmail.com>
2014-11-20 14:24:45 +0700

    socket.io

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 var express = require('express')
+var mongojs = require('mongojs')
+var bodyParser = require('body-parser')
 var app = express()
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var db = mongojs('my_server', ['book']);
 
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json())
 
 app.get('/api/book', function(req, res){
-	var books = [
-		{title: 'Angular', price: 800},
-		{title: 'Node.js', price: 600},
-		{title: 'CSS', price: 500}
-	];
-	res.send(books);
+	db.book.find({}, function(err, docs){
+		res.send(docs);
+	});
 })
 
-var server = app.listen(3000, function () {
+app.post('/api/book', function(req, res){
+	db.book.insert(req.body, function(err, docs){
+		res.send(docs);
+		io.emit("book:refresh");
+	});
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
+http.listen(3000, function () {
 	console.log("server is running")
 })
--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,29 @@
 //app.js
-angular.module("myApp", [])
-.controller('mainCtrl', function($scope, $http){
+angular.module("myApp", ['btford.socket-io'])
+.factory('socketIO', function(socketFactory){
+	 return socketFactory({
+    	ioSocket: io.connect("http://localhost:3000")
+    });
+})
+.controller('mainCtrl', function($scope, $http, socketIO){
 	$scope.books = [];
-	$http.get('/api/book').success(function(data){
-		$scope.books = data;
-	})
+	$scope.bookInstance = {};
+	refreshBooks();
+
+	function refreshBooks(){
+		$http.get('/api/book').success(function(data){
+			$scope.books = data;
+		})
+	}
+
+	$scope.save = function(){
+		$http.post('/api/book', $scope.bookInstance).success(function(data){
+			$scope.books.push(data);
+			$scope.bookInstance = {};
+		});
+	}
+
+	socketIO.on('book:refresh', function(){
+		refreshBooks();
+	});
 })
